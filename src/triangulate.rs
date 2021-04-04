27c3c0a5847ce72cd24bc82ab8b3e47ad63973bf
Matt Keeter <matt.j.keeter@gmail.com>
2021-04-04 09:44:15 -0400

    Add a basic test

--- src/triangulate.rs
+++ src/triangulate.rs
@@ -7,22 +7,22 @@ use ordered_float::OrderedFloat;
 use crate::predicates::{circumcenter, circumradius2, distance2, orient2d, pseudo_angle};
 use crate::Point;
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct PointIndex(usize);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct EdgeIndex(NonZeroUsize);
 
-#[derive(Copy, Clone)]
+#[derive(Copy, Clone, Debug)]
 struct Edge {
     src: PointIndex,
     dst: PointIndex,
     buddy: Option<EdgeIndex>,
 }
 
-
 struct Triangulation<'a> {
     points: &'a[Point],
+    center: Point,
     order: Vec<usize>, // Ordering of the points, from inner to outer
 
     // This stores the start of an edge (as a pseudoangle) as an index into
@@ -34,38 +34,52 @@ struct Triangulation<'a> {
 impl<'a> Triangulation<'a> {
     pub fn new(points: &'a [Point]) -> Triangulation<'a> {
         let seed = Self::seed_triangle(points);
-
+        println!("{:?}", seed);
         let center = circumcenter(points[seed.0], points[seed.1], points[seed.2]);
         let mut pts: Vec<(usize, Point)> = points.iter()
             .cloned()
             .enumerate()
             .collect();
         pts.sort_by_key(|p| OrderedFloat(distance2(center, p.1)));
-        let order: Vec<usize> = pts.iter().map(|p| p.0).collect();
+        let order: Vec<usize> = pts.iter()
+            .map(|p| p.0)
+            .filter(|&i| i != seed.0 && i != seed.1 && i != seed.2)
+            .collect();
 
         let mut out = Triangulation {
             points,
             order,
+            center,
 
             // edges[0] is a reserved slot, so other edges can use None (0)
             // to indicate when they don't have a matched pair.
             edges: vec![Edge { src: PointIndex(0), dst: PointIndex(0), buddy: None }],
             hull: BTreeMap::new(),
         };
-        let e_ab = out.push_edge(PointIndex(seed.0), PointIndex(seed.1), None);
-        let e_bc = out.push_edge(PointIndex(seed.1), PointIndex(seed.2), None);
-        let e_ca = out.push_edge(PointIndex(seed.2), PointIndex(seed.0), None);
 
-        out.hull.insert(OrderedFloat(pseudo_angle(points[seed.0])), e_ab);
-        out.hull.insert(OrderedFloat(pseudo_angle(points[seed.1])), e_bc);
-        out.hull.insert(OrderedFloat(pseudo_angle(points[seed.2])), e_ca);
+        let pa = PointIndex(seed.0);
+        let pb = PointIndex(seed.1);
+        let pc = PointIndex(seed.2);
+
+        let e_ab = out.push_edge(pa, pb, None);
+        let e_bc = out.push_edge(pb, pc, None);
+        let e_ca = out.push_edge(pc, pa, None);
+
+        out.hull.insert(out.key(pa), e_ab);
+        out.hull.insert(out.key(pb), e_bc);
+        out.hull.insert(out.key(pc), e_ca);
 
         out
     }
 
+    fn key(&self, p: PointIndex) -> OrderedFloat<f64> {
+        let p = self.points[p.0];
+        OrderedFloat(crate::predicates::pseudo_angle(
+            (p.0 - self.center.0, p.1 - self.center.1)))
+    }
+
     fn push_edge(&mut self, src: PointIndex, dst: PointIndex, buddy: Option<EdgeIndex>) -> EdgeIndex {
         let n = self.edges.len();
-        // We assume that this edge will produce a new triangle soon
         self.edges.push(Edge {src, dst, buddy});
         EdgeIndex(NonZeroUsize::new(n).unwrap())
     }
@@ -96,14 +110,20 @@ impl<'a> Triangulation<'a> {
                 |q| OrderedFloat(distance2(center, **q)))
             .expect("Could not get initial point");
         let b = pts.iter().enumerate()
-            .filter(|(j, _)| *j != a)
             .position_min_by_key(
-                |(_, p)| OrderedFloat(distance2(pts[a], **p)))
+                |(j, p)| OrderedFloat(if *j == a {
+                    std::f64::INFINITY
+                } else {
+                    distance2(pts[a], **p)
+                }))
             .expect("Could not get second point");
         let c = pts.iter().enumerate()
-            .filter(|(j, _)| *j != a && *j != b)
             .position_min_by_key(
-                |(_, p)| OrderedFloat(circumradius2(pts[a], pts[b], **p)))
+                |(j, p)| OrderedFloat(if *j == a || *j == b {
+                    std::f64::INFINITY
+                } else {
+                    circumradius2(pts[a], pts[b], **p)
+                }))
             .expect("Could not get third point");
 
         if orient2d(pts[a], pts[b], pts[c]) > 0.0 {
@@ -112,5 +132,33 @@ impl<'a> Triangulation<'a> {
             (a, c, b)
         }
     }
+
+    /// Returns the edge of the bounding hull which the given point projects
+    /// onto, as an index into self.edges.
+    fn get_hull_edge(&self, p: PointIndex) -> (f64, Option<EdgeIndex>) {
+        use std::ops::Bound::*;
+        let k = self.key(p);
+        let mut r = self.hull.range((Unbounded, Included(k)));
+        (k.into_inner(), r.next_back().map(|p| *p.1))
+    }
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn basics() {
+        let pts = vec![
+            (0.0, 0.0), (1.0, 0.0), (0.0, 1.0), (2.0, 2.0)
+        ];
+        let t = Triangulation::new(&pts);
+        assert_eq!(t.order.len(), 1);
+        assert_eq!(t.order[0], 3);
+        for i in 0..4 {
+            println!("{}: {:?}, {}", i, pts[i], t.key(PointIndex(i)));
+        }
+        println!("{:?}", t.hull.range((std::ops::Bound::Unbounded,
+                std::ops::Bound::Included(t.key(PointIndex(3))))));
+    }
+}
